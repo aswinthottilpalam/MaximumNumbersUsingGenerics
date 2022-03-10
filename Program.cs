0aec3fac32f31ca94c122f626badc85dd169ef2a
aswin26498 <85921826+aswin26498@users.noreply.github.com>
2022-03-11 00:05:18 +0530

    Maximum of float numbers

--- Program.cs
+++ Program.cs
@@ -9,6 +9,9 @@ namespace MaximumProblemUsingGenerics
             Console.WriteLine(" TO Find Maximum Value Using Generics");
             int value = MaxNumCheck.MaximumIntegerNumber(10, 12, 13);
             Console.WriteLine(value);
+            Console.WriteLine("Find MaxValue Float");
+            double floatValue = MaxNumCheck.MaximumFloatNumber(55.5, 22.2, 33.3);
+            Console.WriteLine(floatValue);
         }
     }
 }
