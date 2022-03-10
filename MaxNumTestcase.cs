37e0ebecc90cc8cfa8b2b668fb0e1c59bf01be17
aswin26498 <85921826+aswin26498@users.noreply.github.com>
2022-03-10 23:18:58 +0530

    Maximum numbers using generics

---
+++ MaxNumTestcase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MaximumProblemUsingGenerics;
+
+namespace MaximumProblemUsingGenerics
+{
+        
+        public class UnitTest1
+        {
+            // Givens the maximum number first position.     
+            
+            public void GivenMaxNumber_FirstPosition()
+            {
+                int result = MaxNumCheck.MaximumIntegerNumber(40, 20, 30);
+                int maxNumber = 40;
+                Assert.AreEqual(maxNumber, result);
+            }
+            /// Givens the maximum number second position.
+
+            
+            public void GivenMaxNumber_SecondPosition()
+            {
+                int result = MaxNumCheck.MaximumIntegerNumber(10, 40, 30);
+                int maxNumber = 40;
+                Assert.AreEqual(maxNumber, result);
+            }
+            /// Givens the maximum number third position.
+
+           
+            public void GivenMaxNumber_ThirdPosition()
+            {
+                int result = MaxNumCheck.MaximumIntegerNumber(10, 20, 40);
+                int maxNumber = 40;
+                Assert.AreEqual(maxNumber, result);
+            }
+        }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MaximumProblemUsingGenerics
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(" TO Find Maximum Value Using Generics");
+            int value = MaximumNumberCheck.MaximumIntegerNumber(10, 12, 13);
+            Console.WriteLine(value);
+        }
+    }
+}
